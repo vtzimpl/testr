bdb7cc4fe913e7db79530138bacab7d4b56a745c
Tzimplakis Vasilis <vtzimpl@yahoo.gr>
2022-01-30 16:20:44 +0200

    fisrst

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      return (
+         <div className="App">
+          <Header/>
+          <Main/>
+          <Footer/>
+         </div>
+      );
+   }
+
+
+
+ function Header() {
+      return (
+         <div className="App-header">
+                   <img src={logo} className="App-logo" alt="logo" />
+        <h2>
+         Vasilis Tzimplakis
+        </h2>
+         </div>
+      );
+   }
+
+
+
+   function Main() {
+      return (
+         <div className="App-main">
+            <h1>this is the main funtion component!!!!!!!</h1>
+         </div>
+      );
+   }
+
+
+   function Footer() {
+      return (
+         <div className="App-footer">
+            <h1>this is the footer funtion component!!!!!!!</h1>
+         </div>
+      );
+   }
+
+
+
+
+
 
 export default App;
